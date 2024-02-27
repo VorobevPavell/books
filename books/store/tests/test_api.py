9023272d84094fba78167f451c618b01371b4e6b
vorobevpasha <korochepashtet@yandex.ru>
2024-02-27 19:15:35 +0300

    write a CRUD tests

--- books/store/tests/test_api.py
+++ books/store/tests/test_api.py
@@ -1,5 +1,9 @@
+import json
 import os
+from decimal import Decimal
+
 import django
+from django.contrib.auth.models import User
 
 from django.urls import reverse
 from rest_framework import status
@@ -15,6 +19,8 @@ django.setup()
 class BooksApiTestCase(APITestCase):
 
     def setUp(self):
+        self.user = User.objects.create(username='test_username')
+
         self.book_1 = Book.objects.create(name='Test book 1', price=25,
                                           author_name='Author1')
         self.book_2 = Book.objects.create(name='Test book 2', price=55,
@@ -23,7 +29,6 @@ class BooksApiTestCase(APITestCase):
                                           author_name='Author3')
 
     def test_get(self):
-
         serializer_data = BookSerializer([self.book_1, self.book_2, self.book_3], many=True).data
         url = reverse('book-list')
         response = self.client.get(url)
@@ -31,7 +36,6 @@ class BooksApiTestCase(APITestCase):
         self.assertEqual(status.HTTP_200_OK, response.status_code)
 
     def test_get_filter(self):
-
         serializer_data = BookSerializer([self.book_2, self.book_3], many=True).data
         url = reverse('book-list')
         response = self.client.get(url, data={
@@ -59,3 +63,53 @@ class BooksApiTestCase(APITestCase):
 
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
+
+    def test_post(self):
+        self.assertEqual(3, Book.objects.all().count())
+        url = reverse('book-list')
+
+        data = {
+
+            'name': 'testbook',
+            'price': '700.00',
+            'author_name': 'Author1'
+
+        }
+        json_data = json.dumps(data)
+        test_user = self.client.force_login(self.user, backend=None)
+        response = self.client.post(url, data=json_data, user=test_user,
+                                    content_type='application/json')
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(4, Book.objects.all().count())
+        self.assertEqual(1, Book.objects.filter(name=data['name'], price=data['price'],
+                                                author_name=data['author_name']).count())
+
+    def test_update(self):
+        url = reverse('book-detail', args=(self.book_1.id,))
+        data = {
+            "name": "Test book 1",
+            "price": "666.00",
+            "author_name": "Author1"
+        }
+        json_data = json.dumps(data)
+
+        user = self.client.force_login(user=self.user)
+
+        response = self.client.put(url, data=json_data,
+                                   content_type='application/json', user=user)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.book_1.refresh_from_db()
+        self.assertEqual(self.book_1.price, Decimal(data['price']))
+
+    def test_delete(self):
+        self.book_test = Book.objects.create(name='Test book test', price=55,
+                                             author_name='Author test')
+
+        book_counter = Book.objects.all().count()
+
+        url = reverse('book-detail', args=(self.book_test.id,))
+        user = self.client.force_login(self.user)
+
+        response = self.client.delete(url, content_type='application/json', user=user)
+        self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
+        self.assertEqual(Book.objects.all().count(), book_counter - 1)
--- books/store/views.py
+++ books/store/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 
 from store.models import Book
@@ -11,7 +11,7 @@ from store.serializers import BookSerializer
 class BooksViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['price']
     search_fields = ['name', 'author_name']
